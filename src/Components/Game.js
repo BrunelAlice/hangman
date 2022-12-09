4d54740bcb1f3796cbfe7a9a8269807c61f44f6a
Brunel Alice <alc.brunel@gmail.com>
2022-12-09 13:00:09 +0000

    Created a state for typed letter

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,40 +1,42 @@
 import { useSelector } from 'react-redux';
+import React, { useState, useEffect } from 'react';
 
 function Game()
 {
-    const word = useSelector((state) => state.word.value);
+    const hiddenWord = useSelector((state) => state.word.value);
+
     // SCREEN
-    const wordArray = [...word];
+    const wordArray = [...hiddenWord];
+
 
     // KEYBOARD
     const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    const [typed, setTyped] = useState("");
 
-    let typed = "";
 
-    function handleClick(e)
-    {
-        e.preventDefault();
-        typed = e.target.innerHTML;
-        if (word.includes(typed))
-        {
-            console.log("You found a letter");
-        }
-    }
 
     return (
         <>
         <div className="screen">
             <div className="screenText">
                     {wordArray.map((letter, index) =>
-                        <div key={index} className="theWord">
-                            <div>{letter}</div>
+                    {
+                        if (letter === typed)
+                        {
+                            return <div key={index} className="theWord">
+                                <div>{letter}</div><div>_</div>
+                            </div>
+                        }
+
+                        return <div key={index} className="theWord">
+                            <div className="hiddenLetter">{letter}</div><div>_</div>
                         </div>
-                    )}
+                    })}
             </div>
         </div>
         <div className="keyboard">
                 {letters.map((key, index) =>
-                    <div className="keys" key={index} onClick={handleClick}>
+                    <div className="keys" key={index} onClick={(e) => setTyped(e.target.innerHTML) }>
                         {key}
                     </div>
                 )}
